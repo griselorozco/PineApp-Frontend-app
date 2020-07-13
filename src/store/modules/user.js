c20322e6795e0683f79db07807205efadca36402
griselorozco <griselorosco@gmail.com>
2020-07-13 16:38:58 -0400

    Arreglos del ESLINT

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,40 +1,40 @@
-import { GET_USER } from "../mutation-types";
+import { GET_USER } from '../mutation-types'
 
-import { getUserById } from "@/api/modules";
+import { getUserById } from '@/api/modules'
 
 const initialState = {
-  found_user: {}
-};
+  found_user: {},
+}
 
 const state = {
-  ...initialState
-};
+  ...initialState,
+}
 
 const getters = {
-  found_user: state => state.found_user
-};
+  found_user: state => state.found_user,
+}
 
 const mutations = {
   [GET_USER]: (state, payload) => {
-    state.found_user = payload.user;
-  }
-};
+    state.found_user = payload.user
+  },
+}
 const actions = {
-  async getUserByIdAction({ commit }, id) {
-    const serviceResponse = await getUserById(id);
-    console.log(serviceResponse);
+  async getUserByIdAction ({ commit }, id) {
+    const serviceResponse = await getUserById(id)
+    console.log(serviceResponse)
     if (serviceResponse.ok) {
-      commit(GET_USER, serviceResponse);
+      commit(GET_USER, serviceResponse)
     } else {
-      const params = { text: serviceResponse.message };
-      window.getApp.$emit("SHOW_ERROR", params);
+      const params = { text: serviceResponse.message }
+      window.getApp.$emit('SHOW_ERROR', params)
     }
-  }
-};
+  },
+}
 
 export default {
   state,
   getters,
   mutations,
-  actions
-};
+  actions,
+}
